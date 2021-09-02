4069b1b7ea3c16b4f5d9d3023f34ce7f872948df
Ali Can <cucu.alican@gmail.com>
2021-09-02 12:15:19 +0300

    vueUrunGirisCikis

---
+++ src/store/actions.js
@@ -0,0 +1,48 @@
+// import Vue from "vue";
+
+// export const setTradeResult = ({ state, commit }, tradeResult) => {
+//   commit("updateTradeResult", tradeResult);
+//   let tradeData = {
+//     purchase: state.purchase,
+//     sale: state.sale,
+//   };
+//   Vue.http
+//     .put(
+//       "https://urun-islemleri-prod-82130-default-rtdb.firebaseio.com/trade-result.json",
+//       tradeData
+//     )
+//     .then((response) => {
+//       console.log(response);
+//       console.log(state.purchase);
+//       console.log(state.sale);
+//       console.log(state.balance);
+//     });
+// };
+
+import Vue from "vue";
+
+export const setTradeResult = ({ state, commit }, tradeResult) => {
+  commit("updateTradeResult", tradeResult);
+  let tradeData = {
+    purchase: state.purchase,
+    sale: state.sale,
+  };
+  Vue.http
+    .put(
+      "https://urun-islemleri-prod-82130-default-rtdb.firebaseio.com/trade-result.json",
+      tradeData
+    )
+    .then((response) => {
+    });
+};
+
+export const getTradeResult = ({ commit }) => {
+  Vue.http
+    .get(
+      "https://urun-islemleri-prod-82130-default-rtdb.firebaseio.com/trade-result.json"
+    )
+    .then((response) => {
+      console.log(response);
+      commit("updateTradeResult",response.body)
+    });
+};
